a4ebd999031659bb5625cef8f367fafef9ee1ce4
kkll497 <2447536129@qq.com>
2025-11-23 16:33:57 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,6 +3,7 @@ from jmcomic.cl import JmcomicUI
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = str_to_list('''
+206537
 625305
 590827
 79575
@@ -273,7 +274,6 @@ jm_albums = str_to_list('''
 525770
 534640
 534573
-206537
 
 ''')
 
